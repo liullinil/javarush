9a147bfe033f45a61b393b9fc066cd446a3240b8
hazrat <hazrat@sprut.ru>
2021-10-17 20:58:05 +0300

    Init

--- Task1/src/main/java/com/codenostra/javarush/task1/SolarSystem.java
+++ Task1/src/main/java/com/codenostra/javarush/task1/SolarSystem.java
@@ -9,5 +9,14 @@ package com.codenostra.javarush.task1;
  * @author hazrat
  */
 public class SolarSystem {
-    
+
+    public static long age = 4_568_200_000L;
+
+    public static int planetsCount = 8;
+
+    public static int starsCount = 1;
+
+    public static String starName = "Солнце";
+
+    public static int galacticCenterDistance = 27170;
 }
