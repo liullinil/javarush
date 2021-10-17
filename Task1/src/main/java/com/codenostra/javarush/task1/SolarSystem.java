4e1e423671520c6b5129aecb1ffa9e43420667b9
hazrat <hazrat@sprut.ru>
2021-10-17 20:49:51 +0300

    Init

---
+++ Task1/src/main/java/com/codenostra/javarush/task1/SolarSystem.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codenostra.javarush.task1;
+
+/**
+ *
+ * @author hazrat
+ */
+public class SolarSystem {
+    
+}
